97291f0be6c45c59f504c67b325f0a381d88b750
mahizaman490 <mahizaman490@gmail.com>
2023-09-20 09:34:14 +0600

    added some styles for users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,12 +1,13 @@
 import { useLoaderData } from "react-router-dom";
 import User from "../User/User";
+import './Users.css'
 const Users = () => {
     const users = useLoaderData()
     return (
         <div>
             <h2>Our Users {users.length}</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum minus et ratione perferendis incidunt ea molestias officiis porro, magni repudiandae?</p>
-            <div>
+            <div className="users">
                 {
                     users.map(user => <User key={user.id} user={user}></User>)
                 }
